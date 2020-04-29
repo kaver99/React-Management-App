b8bbce56e15bc48e458e49390b709dfc2e548292
kaver99 <kaver99@gmail.com>
2020-04-30 00:39:49 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -2,6 +2,29 @@ import React, { Component } from 'react';
 import Customer from './components/Customer';
 import './App.css';
 
+// material-ui 설정
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+
+// css 설정
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing(0.3),
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+})
+
+
 const customers = [{
   'id': 1,
   'image': 'https://placeimg.com/64/64/1',
@@ -30,27 +53,34 @@ const customers = [{
 
 class App extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        {
-          customers.map(c => {
-            return (
-              // map 함수 이용 시 key 정의 필수
-              <Customer
-                key={c.id}
-                id={c.id}
-                image={c.image}
-                name={c.name}
-                birthday={c.birthday}
-                gender={c.gender}
-                job={c.job}
-              />
-            );
-          })
-        }
-      </div>
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+          { 
+            customers.map(c => {
+              return (
+                // map 함수 이용 시 key 정의 필수
+                <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
+              );
+            })
+          }
+          </TableBody>
+        </Table>
+      </Paper>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
