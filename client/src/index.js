557d3cb610e20e9adc2796bf03eea1f08af87321
kaver99 <kaver99@gmail.com>
2020-05-13 09:35:06 +0900

    [20190513][ie11 compatible]

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// ie 호환성 설정
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+
 ReactDOM.render(
   // <React.StrictMode>
     <App />,
